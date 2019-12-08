c51c6ad9a23837e4dca47effae448e4b4f3af1aa
Anhfpoly <anhfpoly@gmail.com>
2019-12-08 13:17:28 +0700

    Bắt đầu làm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, {Component} from 'react';
 import AppContainer from './navigation/route';
-import database from '@react-native-firebase/database';
+
 export default class App extends Component {
-  componentDidMount() {
-    this._getABC()
-  }
-  _getABC = async() => {
-    const ref = database().ref(`/users/`);
-    const snapshot = await ref.once('value');
-    console.log(snapshot.val())
-  }
   render() {
-    return <AppContainer />;
+    return <AppContainer/>;
   }
 }
--- src/screens/RegVehicle/index.js
+++ src/screens/RegVehicle/index.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
 import {Text, View, SafeAreaView} from 'react-native';
 import {TextInput, Header} from '../../components';
+import database from '@react-native-firebase/database';
 
 export default class RegVehicle extends Component {
   static navigationOptions = {header: null};
+  componentDidMount() {
+    this._getValue();
+    this._setValue();
+  }
+  _getValue = async () => {
+    const ref = database().ref('/users/');
+    const snapshot = await ref.once('value');
+    console.log(snapshot.val());
+  };
+  _setValue = async () => {
+    const ref = database().ref('users');
+    console.log('New record key:', ref.key);
+    await ref.child('test').set({
+      first: 'Ada 24',
+      last: 'Lovelace 236',
+    });
+  };
   render() {
     return (
       <SafeAreaView>
