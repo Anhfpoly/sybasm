9b4f99efbe38fb450ee84dcfb90dafa95e9d5dec
Anhfpoly <anhfpoly@gmail.com>
2019-12-09 11:50:38 +0700

    Chi tiết biên bản

--- src/screens/VehicleNoti/index.js
+++ src/screens/VehicleNoti/index.js
@@ -7,16 +7,29 @@ import {
   ScrollView,
   Platform,
   PermissionsAndroid,
+  StyleSheet,
 } from 'react-native';
 import {TextInput, Header} from '../../components';
 import database from '@react-native-firebase/database';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import {Fumi} from 'react-native-textinput-effects';
+import Dialog, {
+  DialogFooter,
+  DialogButton,
+  ScaleAnimation,
+  DialogContent,
+} from 'react-native-popup-dialog';
+import {bold} from 'ansi-colors';
 
 export default class VehicleNoti extends Component {
   static navigationOptions = {header: null};
   state = {
     danhsachvipham: [],
+    visible: false,
+    mabienban: '',
+  };
+  showDialog = () => {
+    this.setState({visible: true});
   };
   componentDidMount() {
     this._getDSViPham('61D');
@@ -36,9 +49,7 @@ export default class VehicleNoti extends Component {
     });
   };
   _notidetail(value) {
-    return <View>
-        <Text>{value.mabienban}</Text>
-    </View>;
+    alert(value.mabienban);
   }
   render() {
     return (
@@ -66,7 +77,7 @@ export default class VehicleNoti extends Component {
                   }}
                   key={index}
                   onPress={() => {
-                    this._notidetail(item);
+                    this.setState({visible: true, mabienban: item.mabienban});
                   }}>
                   <Text
                     numberOfLines={1}
@@ -103,6 +114,53 @@ export default class VehicleNoti extends Component {
                   </View>
                 </TouchableOpacity>
               ))}
+              <View>
+                <Dialog
+                  visible={this.state.visible}
+                  onTouchOutside={() => {
+                    this.setState({visible: false});
+                  }}
+                  footer={
+                    <DialogFooter>
+                      <DialogButton text="Sai" onPress={() => {}} />
+                      <DialogButton text="Đúng" onPress={() => {}} />
+                    </DialogFooter>
+                  }
+                  dialogAnimation={
+                    new ScaleAnimation({
+                      slideFrom: 'bottom',
+                    })
+                  }>
+                  <DialogContent>
+                    <View
+                      style={{justifyContent: 'center', alignItems: 'center'}}>
+                      <Text style={{marginTop: 10, fontWeight: 'bold'}}>
+                        CỘNG HOÀ XÃ HỘI CHỦ NGHĨA VIỆT NAM
+                      </Text>
+                      <Text style={{fontWeight: 'bold'}}>
+                        Độc lập - Tự Do - Hạnh Phúc
+                      </Text>
+                      <Text style={{marginTop: 6}}>{'******'}</Text>
+                      <Text style={{fontSize: 12, fontWeight: 'bold'}}>
+                        BIÊN BẢN VI PHẠM HÀNH CHÍNH
+                      </Text>
+                      <Text style={{fontSize: 12, fontWeight: 'bold'}}>
+                        Về lĩnh vực giao thông đường bộ
+                      </Text>
+                    </View>
+                    <View style={{marginTop: 10}}>
+                      <Text style={{fontSize: 12, color: '#ff4800'}}>
+                        {'Mã biên bản: '}
+                        {this.state.mabienban}
+                      </Text>
+                      <Text style={{fontSize: 12}}>
+                        Căn cứ: luật xử lý vi phạm hành chính
+                      </Text>
+                      <Text style={{fontSize: 12}}>Ngày: </Text>
+                    </View>
+                  </DialogContent>
+                </Dialog>
+              </View>
             </View>
           )}
         </ScrollView>
@@ -110,3 +168,28 @@ export default class VehicleNoti extends Component {
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 10,
+    backgroundColor: '#ecf0f1',
+  },
+
+  closeButtonContainer: {
+    position: 'absolute',
+    top: 51,
+    width: 44,
+    height: 44,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+  },
+
+  closeButton: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
